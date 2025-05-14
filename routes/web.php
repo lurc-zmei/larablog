7aea91fd26ba3a22cf3d977a7c8fbb667ee8a742
lurc-zmei <lurc-zmei@inbox.ru>
2025-05-14 19:59:08 +0300

    Начало контроллеров и связывание их с маршрутами

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+
+/*
 Route::get('/', function () {
     return view('index');
 });
@@ -9,3 +12,7 @@ Route::get('/', function () {
 Route::get('/item', function () {
     return view('post.item');
 });
+*/
+
+Route::get('/', [PostController::class, 'index']);
+Route::get('/item', [PostController::class, 'show']);
